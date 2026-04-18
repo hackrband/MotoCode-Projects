1ed7917425b414267daf9fbc9155771ef7aa4ae4
HΛCKRBΛND <138342638+hackrband@users.noreply.github.com>
2026-04-18 14:12:39 -0400

    Bike Stat Finder Project Initial Push

---
+++ main.py
@@ -0,0 +1,103 @@
+while True:
+
+    user_input = input("\nEnter a bike model for stats: ")
+
+    if user_input == "BMW S1000RR":
+        print("\n",user_input, " Stats: ")
+        print(" Top Speed: 188 MPH (200+ MPH without limitor)")
+        print(" 0-60 mph: 2.3 - 3.2 Seconds")
+        print(" Max Rev: 15,100 rpm")
+        print(" MSRP: $19,895")
+
+    elif user_input == "Yamaha YZF-R1":
+        print("\n",user_input, " Stats: ")
+        print(" Top Speed: 186 MPH")
+        print(" 0-60 mph: 2.6 Seconds")
+        print(" Max Rev: 14,500 rpm")
+        print(" MSRP: $17,999")
+
+    elif user_input == "Kawasaki Ninja ZX-10R":
+        print("\n",user_input, " Stats: ")
+        print(" Top Speed: 186 MPH")
+        print(" 0-60 mph: 2.8 Seconds")
+        print(" Max Rev: 14,000 rpm")
+        print(" MSRP: $16,399")
+
+    elif user_input == "Honda CBR1000RR-R":
+        print("\n",user_input, " Stats: ")
+        print(" Top Speed: 186 MPH")
+        print(" 0-60 mph: 2.9 Seconds")
+        print(" Max Rev: 15,000 rpm")
+        print(" MSRP: $28,900")
+
+    elif user_input == "Ducati Panigale V4":
+        print("\n",user_input, " Stats: ")
+        print(" Top Speed: 199 MPH")
+        print(" 0-60 mph: 2.6 Seconds")
+        print(" Max Rev: 14,500 rpm")
+        print(" MSRP: $23,295")
+
+    elif user_input == "Suzuki GSX-R1000":
+        print("\n",user_input, " Stats: ")
+        print(" Top Speed: 186 MPH")
+        print(" 0-60 mph: 2.8 Seconds")
+        print(" Max Rev: 14,500 rpm")
+        print(" MSRP: $16,149")
+
+    elif user_input == "Kawasaki Ninja H2R":
+        print("\n",user_input, " Stats: ")
+        print(" Top Speed: 249+ MPH")
+        print(" 0-60 mph: 2.5 Seconds")
+        print(" Max Rev: 14,000 rpm")
+        print(" MSRP: $55,000")
+
+    elif user_input == "BMW M1K":
+        print("\n",user_input, " Stats: ")
+        print(" Top Speed: 189 MPH")
+        print(" 0-60 mph: 2.6 Seconds")
+        print(" Max Rev: 15,100 rpm")
+        print(" MSRP: $32,995")
+
+    elif user_input == "BMW S1K":
+        print("\n",user_input, " Stats: ")
+        print(" Top Speed: 188 MPH")
+        print(" 0-60 mph: 2.7 Seconds")
+        print(" Max Rev: 14,600 rpm")
+        print(" MSRP: $17,895")
+
+    elif user_input == "Ducati Pangeal v4":
+        print("\n",user_input, " Stats: ")
+        print(" Top Speed: 199 MPH")
+        print(" 0-60 mph: 2.6 Seconds")
+        print(" Max Rev: 14,500 rpm")
+        print(" MSRP: $23,295")
+
+    elif user_input == "Kawasaki Ninja 600":
+        print("\n",user_input, " Stats: ")
+        print(" Top Speed: 155 MPH")
+        print(" 0-60 mph: 3.2 Seconds")
+        print(" Max Rev: 16,000 rpm")
+        print(" MSRP: $11,999")
+
+    elif user_input == "Yamaha R1":
+        print("\n",user_input, " Stats: ")
+        print(" Top Speed: 186 MPH")
+        print(" 0-60 mph: 2.6 Seconds")
+        print(" Max Rev: 14,500 rpm")
+        print(" MSRP: $17,999")
+
+    elif user_input == "Yamaha R6":
+        print("\n",user_input, " Stats: ")
+        print(" Top Speed: 160 MPH")
+        print(" 0-60 mph: 3.0 Seconds")
+        print(" Max Rev: 16,500 rpm")
+        print(" MSRP: $12,199")
+
+    elif user_input.upper() == "DONE":
+        break
+    else:
+        print("Try the following models: \n BMW S1000RR \n Yamaha YZF-R1 \n Kawasaki Ninja ZX-10R \n Honda CBR1000RR-R \n Ducati Panigale V4 \n Suzuki GSX-R1000 \n Kawasaki Ninja H2R \n BMW M1K \n BMW S1K \n Ducati Pangeal v4 \n Kawasaki Ninja 600 \n Yamaha R1 \n Yamaha R6 \n")
+
+
+if user_input == "Done" or user_input == "done":
+    print("\nIf you have any suggestions, please email me: \nm.salim.nasirov@gmail.com")
